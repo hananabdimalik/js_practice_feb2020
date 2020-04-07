59d4dcc9d5a3c95bb0fa77cfaee7933448174d9b
Hanan Abdimalik <hanan.abdimalok@gmail.com>
2020-04-07 22:31:05 +0100

    solved findNextNumber, SumArray and reverseNumber

--- challenges/week8.js
+++ challenges/week8.js
@@ -1,38 +1,139 @@
 const findNextNumber = (nums, n) => {
-  if (nums === undefined) throw new Error("nums is required");
-  if (n === undefined) throw new Error("n is required");
-  // Your code here!
-};
+
+  for (let i = 0; i < nums.length - 1; i++) {
+
+    if (nums[i] === n) {
+      console.log(nums[i + 1])
+      return nums[i + 1]
+    }
+  }
+  return null
+
+}
+
+//loop through the array
+//if the value of n is same as the value of nums[i], add one to the index to get the next one
+//then return the next one in the list
+
+
+// if (nums === undefined) throw new Error("nums is required");
+// if (n === undefined) throw new Error("n is required");
+// Your code here!
+
 
 const count1sand0s = str => {
-  if (str === undefined) throw new Error("str is required");
-  // Your code here!
+  let obj = {}
+  let newStr = str.split('|')
+  console.log(newStr)
+  let count = 0
+  for (let i = 0; i < newStr.length; i++) {
+    if (newStr[i] === '1') {
+      count++
+      console.log(count)
+
+    // }
+    // count2nd = 0
+    // if (str[i] === 0) {
+    //   count2nd++
+    // }
+  }
+}
+
+  //create an a var that holds an empty obj
+  //convert string to array via split method 
+  //loop through the array
+  // if str === number, count ++
+  //push results back to an object
+
+
+  // if (str === undefined) throw new Error("str is required");
+  // // Your code here!
 };
 
 const reverseNumber = n => {
-  if (n === undefined) throw new Error("n is required");
+  return parseFloat(n.toString().split('').reverse().join(''))
+
+  //convert number to string to split, reverse, and join
+  //use parseFloat to convert string to number
   // Your code here!
-};
+}
 
 const sumArrays = arrs => {
-  if (arrs === undefined) throw new Error("arrs is required");
-  // Your code here!
+  return arrs.toString().split(',').map(Number).reduce((a, b) => (a + b), 0)
 };
+//convert numbers to string and split
+//use map method to give total of the numbers in the array 
+//use reduce total of first array to the next, to the next, to the next
+// use reduced method to add the numbers 
+
+
+// if (arrs === undefined) throw new Error("arrs is required");
+// Your code here! const reduced= (accumulator, currentValue) => accumulator + currentValue;
+
 
 const arrShift = arr => {
-  if (arr === undefined) throw new Error("arr is required");
+
+  let firstNumber = arr[0]
+  let lastNumber = arr.slice(-1)[0]
+  let temp = 
+ temp = lastNumber;
+lastNumber = firstNumber 
+
+  console.log(firstNumber)
+  // let swapNumber = firstNumber
+  // let swapNumberTwo = lastNumber
+  // console.log(lastNumber)
+
+  //store index 1 in a var
+  //store the last value in another var
+  //assign temp with the last value
+  //then assign first value the last value 
+
+  // if (arr === undefined) throw new Error("arr is required");
   // Your code here!
 };
 
 const findNeedle = (haystack, searchTerm) => {
-  if (haystack === undefined) throw new Error("haystack is required");
-  if (searchTerm === undefined) throw new Error("searchTerm is required");
+
+  // for (searchTerm in haystack){
+  for (searchTerm in haystack) {
+    if (haystack.includes(searchTerm) === " ") {
+      return true
+    }
+
+  }
+  // loop through an object via for in method
+  // using conditional statement, if condition is met, return true
+  //else return false
+
+  // if (haystack === undefined) throw new Error("haystack is required");
+  // if (searchTerm === undefined) throw new Error("searchTerm is required");
   // Your code here!
 };
 
 const getWordFrequencies = str => {
-  if (str === undefined) throw new Error("str is required");
-  // Your code here!
+  let obj = {};
+
+  // convert string to array then remove quoates
+  // loop through the array
+  //start count from 0
+  // in an if statement, if word[i]=== word[i], count on 
+  //push into object word, count
+  console.log(arr)
+  // let count = 0;
+
+  // for (let i=0; i < str.length; i++){
+  //   if ()
+  // } else {
+  //   let value = '${count}'${}
+  // }
+
+  // return str.split(' ').length
+
+
+  // if (str === undefined) throw new Error("str is required");
+  // // Your code here!
+
 };
 
 module.exports = {
