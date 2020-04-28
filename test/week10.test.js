f0f4890a44ebf890004597ad063822333028e1d7
Hanan Abdimalik <hanan.abdimalok@gmail.com>
2020-04-28 22:39:40 +0100

    added a test for createRange

--- test/week10.test.js
+++ test/week10.test.js
@@ -25,5 +25,23 @@ describe("sumDigits", () => {
     });
 });
 
+describe("createRange", () => {
 
+    test("it throws an error if the start number is not passed", () => {
+        expect(() => {
+            createRange();
+        }).toThrow("start is required")
+    })
+    test("it throws an error if the end number is not passed", () => {
+        expect(() => {
+            createRange('');
+        }).toThrow("end is required")
+    })
+    test("returns an array of numbers", () => {
+        expect(createRange(2, 10, 2)).toEqual([2, 4, 6, 8, 10])
+    })
+    test("returns an array of numbers", () => {
+        expect(createRange(3, 21, 3)).toEqual([3, 6, 9, 12, 15, 18, 21])
+    });
+});
 
