8cbf5ce3ce34abf43fcbb39e66c2ba182c964e22
Hanan Abdimalik <hanan.abdimalok@gmail.com>
2020-04-28 22:41:34 +0100

    added a test for getScreenTimeAlertList

--- test/week10.test.js
+++ test/week10.test.js
@@ -45,3 +45,31 @@ describe("createRange", () => {
     });
 });
 
+describe("getScreenTimeAlertList", () => {
+    test("returns an array of usernames of users who have used more than 100 minutes of screentime for a given date", () => {
+        expect(getScreentimeAlertList(
+            [
+                {
+                    username: "beth_1234",
+                    name: "Beth Smith",
+                    screenTime: [
+                        { date: "2019-05-01", usage: { twitter: 34, instagram: 22, facebook: 40 } }, // 96
+                        { date: "2019-05-02", usage: { twitter: 56, instagram: 40, facebook: 31 } }, //127
+                        { date: "2019-05-03", usage: { twitter: 12, instagram: 15, facebook: 19 } }, //46
+                        { date: "2019-05-04", usage: { twitter: 10, instagram: 56, facebook: 61 } }, //127
+                    ]
+                },
+                {
+                    username: "sam_j_1989",
+                    name: "Sam Jones",
+                    screenTime: [
+                        { date: "2019-06-11", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 10 } },//10
+                        { date: "2019-06-13", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 16 } },//16
+                        { date: "2019-06-14", usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 31 } },//31
+                    ]
+                },
+            ])
+        ).toBe(["beth_1234"])
+    });
+});
+
